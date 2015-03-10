a8874f597d7613d29884df703e1c5bd27fcc1da1
luckysahani <luckythegreat4@gmail.com>
2015-03-10 18:47:21 +0530

    wroking on compilationunit

--- parser.py
+++ parser.py
@@ -1,133 +1,125 @@
 #!/usr/bin/env python
 
-import ply.lex as lex
-from ply.lex import TOKEN
-import sys
 import ply.yacc as yacc
 
-class MyLexer(object):
-	# 	
-	# 
-	# 
-	# 
-	# 
-	# Our lexer code is here
-	# 
-	# 
-	# 
-	# 
-	# 
+from lexer import tokens
 
-class Expression_Parser(object):
+def p_Identifier(p):
+    'Identifier : IDENTIFIER'
 
+def p_QualifiedIdentifier(p):
+    '''QualifiedIdentifier : Identifier | Identifier . QualifiedIdentifier'''
 
+def p_QualifiedIdentifierList(p): 
+    ''' QualifiedIdentifierList : QualifiedIdentifier | QualifiedIdentifier , QualifiedIdentifierList  '''
 
+def p_CompilationUnit(p): 
+	''' CompilationUnit : Sq '''
 
-class Statement_Parser(object):
+def p_Square_Annotations(p):
+	''' Square_Annotations : Annotations |  '''
 
+def p_Curly_ImportDeclaration(p):
+	'''  Curly_ImportDeclaration : ImportDeclaration | ImportDeclaration Curly_ImportDeclaration '''
 
-
-
-
-class Name_Parser(object):
+def p_Curly_TypeDeclaration(p):
+	'''  Curly_TypeDeclaration : TypeDeclaration | TypeDeclaration Curly_TypeDeclaration '''
 
 
 
+    [[Annotations] package QualifiedIdentifier ;]
+                                {ImportDeclaration} {TypeDeclaration}
 
-class Literal_Parser(object):
+ImportDeclaration: 
+    import [static] Identifier { . Identifier } [. *] ;
+
+TypeDeclaration: 
+    ClassOrInterfaceDeclaration
+    ;
+
+ClassOrInterfaceDeclaration: 
+    {Modifier} (ClassDeclaration | InterfaceDeclaration)
+
+ClassDeclaration: 
+    NormalClassDeclaration
+    EnumDeclaration
+
+InterfaceDeclaration: 
+    NormalInterfaceDeclaration
+    AnnotationTypeDeclaration
+
+
+
+NormalClassDeclaration: 
+    class Identifier [TypeParameters]
+                                [extends Type] [implements TypeList] ClassBody
+
+EnumDeclaration:
+    enum Identifier [implements TypeList] EnumBody
+
+NormalInterfaceDeclaration: 
+    interface Identifier [TypeParameters] [extends TypeList] InterfaceBody
+
+AnnotationTypeDeclaration:
+    @ interface Identifier AnnotationTypeBody
+
+Type:
+    BasicType {[]}
+    ReferenceType  {[]}
+
+BasicType: 
+    byte
+    short
+    char
+    int
+    long
+    float
+    double
+    boolean
+
+ReferenceType:
+    Identifier [TypeArguments] { . Identifier [TypeArguments] }
+
+TypeArguments: 
+    < TypeArgument { , TypeArgument } >
+
+TypeArgument:  
+    ReferenceType
+    ? [ (extends | super) ReferenceType ]
 
 
 
 
-class Type_Parser(object):
-
-
-
-
-class Class_Parser(object):
 
 
 
 
 
-class CompilationUnit_Parser(object):
 
 
 
 
 
-class MyParser(Expression_Parser, Statement_Parser, Name_Parser, Literal_Parser, Type_Parser, Class_Parser, CompilationUnit_Parser):
 
-    tokens = MyLexer.tokens
 
-    def p_goal_compilation_unit(self, p):
-        '''goal : PLUSPLUS compilation_unit'''
-        p[0] = p[2]
 
-    def p_goal_expression(self, p):
-        '''goal : MINUSMINUS expression'''
-        p[0] = p[2]
 
-    def p_goal_statement(self, p):
-        '''goal : '*' block_statement'''
-        p[0] = p[2]
 
-    def p_error(self, p):
-        print('error: {}'.format(p))
 
-    def p_empty(self, p):
-        '''empty :'''
 
-# class Parser(object):
 
-#     def __init__(self):
-#         self.lexer = lex.lex(module=MyLexer(), optimize=1)
-#         self.parser = yacc.yacc(module=MyParser(), start='goal', optimize=1)
 
-#     def tokenize_string(self, code):
-#         self.lexer.input(code)
-#         for token in self.lexer:
-#             print(token)
 
-#     def tokenize_file(self, _file):
-#         if type(_file) == str:
-#             _file = open(_file)
-#         content = ''
-#         for line in _file:
-#             content += line
-#         return self.tokenize_string(content)
 
-#     def parse_expression(self, code, debug=0, lineno=1):
-#         return self.parse_string(code, debug, lineno, prefix='--')
 
-#     def parse_statement(self, code, debug=0, lineno=1):
-#         return self.parse_string(code, debug, lineno, prefix='* ')
 
-#     def parse_string(self, code, debug=0, lineno=1, prefix='++'):
-#         self.lexer.lineno = lineno
-#         return self.parser.parse(prefix + code, lexer=self.lexer, debug=debug)
+parser = yacc.yacc()
 
-#     def parse_file(self, _file, debug=0):
-#         if type(_file) == str:
-#             _file = open(_file)
-#         content = _file.read()
-#         return self.parse_string(content, debug=debug)
-
-lexer = lex.lex(module=MyLexer())
-parser = yacc.yacc(module=MyParser(), write_tables=0, start='type_parameters')
-file=(open(sys.argv[1],'r')).read()
-# file = [
-#         '<T extends Foo & Bar>'
-#     ]
-
-for expr in file:
-    print('lexing expression {}'.format(expr))
-    lexer.input(expr)
-    for token in lexer:
-        print(token)
-
-    print('parsing expression {}'.format(expr))
-    t = parser.parse(expr, lexer=lexer)
-    # t = parser.parse(expr, lexer=lexer, debug=1)
-    print('result: {}'.format(t))
-    print('--------------------------------')
+while True:
+   try:
+       s = raw_input('Input:')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print result
